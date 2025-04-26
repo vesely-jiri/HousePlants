2b53880376bd78026d5892087a26ba348dd3393e
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-26 20:22:40 +0200

    Readded files to git, to fix duplicated DebugManager

--- src/cz/jpcz/houseplants/Main.java
+++ src/cz/jpcz/houseplants/Main.java
@@ -4,7 +4,7 @@ import cz.jpcz.houseplants.test.DataTest;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Executing program in first portfolio project...");
+        System.out.println("Executing program in first portfolio project: Houseplants");
 
         DataTest.run();
     }
